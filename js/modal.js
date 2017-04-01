c0867e16ebf09a86252178a91816b9bd7714c8a7
ericaylward <eric.t.aylward@gmail.com>
2017-04-01 02:38:07 -0400

    triggers added for modals - not yet on divs
    3 buttons to trigger the 3 modals - youll need to modify 'modal.css' to
    edit the modals --- my apologies for how nasty the code/modals are atm
    (I actually spent a bit cleaning it up too)

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,29 @@
-var mapRed = "rgb(238,64,54)";
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+var items = document.querySelectorAll(".timeline li");
+ 
+// code for the isElementInViewport function
+ 
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  };
+}
+
+});
